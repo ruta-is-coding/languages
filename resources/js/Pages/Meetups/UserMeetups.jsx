5a9eb469b77ece86ddc7f02424ae05ddcca54070
ruta-is-coding <rutajurg.123@gmail.com>
2023-10-24 16:53:09 +0300

    Added QR codes to contacts

--- resources/js/Pages/Meetups/UserMeetups.jsx
+++ resources/js/Pages/Meetups/UserMeetups.jsx
@@ -1,19 +1,16 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import Container from "@/Components/Container";
 import { Head } from "@inertiajs/react";
-import MeetupProvider from "./MeetupProvider";
 
 const UserMeetups = ({ auth }) => {
     return (
         <AuthenticatedLayout user={auth.user}>
-            <MeetupProvider>
-                <Head title="My meetups" />
-                <section>
-                    <Container>
-                        <h1>My meetups</h1>
-                    </Container>
-                </section>
-            </MeetupProvider>
+            <Head title="My meetups" />
+            <section>
+                <Container>
+                    <h1>My meetups</h1>
+                </Container>
+            </section>
         </AuthenticatedLayout>
     );
 };
