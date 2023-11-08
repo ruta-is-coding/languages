da53d69bc49b0522ebfc3c8eefa1d3cadc968e85
ruta-is-coding <rutajurg.123@gmail.com>
2023-11-08 18:45:17 +0200

    Edited colours

--- resources/js/Components/Success.jsx
+++ resources/js/Components/Success.jsx
@@ -1,8 +1,21 @@
 const Success = ({ flash }) => {
     return (
         <div className="flex justify-center animate__animated animate__fadeIn mb-5">
-            <div className="border border-lime-400 rounded bg-lime-200 px-4 py-3 w-full max-w-md">
-                <p className="text-center text-lime-700">{flash.message}</p>
+            <div
+                className="border rounded px-4 py-3 w-full max-w-md"
+                style={{
+                    background: "#D6FF75",
+                    borderColor: "#abcc5e",
+                }}
+            >
+                <p
+                    className="text-center"
+                    style={{
+                        color: "#6b803b",
+                    }}
+                >
+                    {flash.message}
+                </p>
             </div>
         </div>
     );
